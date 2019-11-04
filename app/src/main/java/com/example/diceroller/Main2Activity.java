0022a18b2721d0c65767939b3737c46706d31ec2
Joshlan Raposo <1801686@brunel.ac.uk>
2019-11-04 20:02:28 +0000

    Bonus Part 4.5 completed

---
+++ app/src/main/java/com/example/diceroller/Main2Activity.java
@@ -0,0 +1,50 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+    EditText inputRule;
+    MainActivity access;
+    Intent intent2, intent3;
+    Button diceRooler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+// Part 3a
+
+    public void addingToArray(View view){
+        TextView tv = this.findViewById(R.id.textView2);
+        EditText inputRule = this.findViewById(R.id.newRule);
+        ArrayList<String> questions1 = new ArrayList<String>();
+        String s = inputRule.getText().toString();
+        questions1.add(s);
+        tv.setText(s);
+
+    }
+
+    public void bkScreenSave(View view){
+        intent2 = new Intent(this,MainActivity.class);
+        startActivity(intent2);
+    }
+
+    public void bkScreenCancel(View view) {
+        intent3 = new Intent(this, MainActivity.class);
+        startActivity(intent3);
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diceroller;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,18 +12,24 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView dice;
+    TextView dice2;
     EditText InputNumber;
     TextView point;
+    String num;
     int count = 0;
+    ArrayList<String> questions;
 
+    public static final String EXTRA_MESSAGE = "com.example.DiceRoller.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,21 +68,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void clickTheButton(View view) {
 
-        dice = this.findViewById(R.id.textView);
-        point = this.findViewById(R.id.userPoints);
-
+    public int roll_the_dice() {
         Random rand = new Random();
-        int number = rand.nextInt(6)+1;
-        dice.setText(Integer.toString(number));
-        InputNumber = (EditText) this.findViewById(R.id.roll);
-        String num = InputNumber.getText().toString();
-        if(num.equals(Integer.toString(number))&&Integer.parseInt(num) < 7) {
-            dice.setText("Congratulations: " + number);
-            count++;
-            point.setText("User Ponits: " + count);
-        }
+        int number = rand.nextInt(6) + 1;
+        return number;
+    }
+//Part 1 A,B,C
+public void clickTheButton(View view) {
+    int number = roll_the_dice();
+    dice = this.findViewById(R.id.textView);
+    point = this.findViewById(R.id.userPoints);
+    dice.setText(Integer.toString(number));
+    InputNumber = this.findViewById(R.id.roll);
+    num = InputNumber.getText().toString();
+    if (num.equals(Integer.toString(number)) && Integer.parseInt(num) < 7) {
+        dice.setText("Congratulations: " + number);
+        iceBreakerRule();
+        count++;
+        point.setText("User Ponits: " + count);
+
+    }
+}
+    //Part 3a
+    public void iceBreakerRule(){
+        dice2 = this.findViewById(R.id.textView3);
+        int number2 = roll_the_dice()-1;
+        questions = new ArrayList<String>();
+        questions.add("If you could go anywhere in the world, where would you go?");
+        questions.add("If you were stranded on a desert island, what three things would you want to take with you?");
+        questions.add("If you could eat only one food for the rest of your life, what would that be?");
+        questions.add("If you won a million dollars, what is the first thing you would buy?");
+        questions.add("If you could spend the day with one fictional character, who would it be?");
+        questions.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+        dice2.setText(questions.get(number2));
+    }
+
+    public void clickTheSecondButton(View view){
+        iceBreakerRule();
+    }
+    //bonus part 4.5
+    public void secondScreen(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
 
